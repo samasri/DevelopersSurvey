c07f796c6757e135966ac94bc195c795f6a77973
Sarah Nadi <nadi@ualberta.ca>
2018-12-07 18:13:21 +0000

    Add dummy sentence to each thread

--- dataImported/converter.py
+++ dataImported/converter.py
@@ -1,9 +1,11 @@
 # Converts the CSV file to Ruby commands. Built for Python 2.7
 
+
 import csv
 
-with open('sampleData.csv', 'rb') as f:
+with open('finaljson.csv', 'rb') as f:
 	reader = csv.reader(f)
+	lastSeen = ""
 	for row in reader:
 		technique = row[0]
 		threadID = row[1]
@@ -11,7 +13,11 @@ with open('sampleData.csv', 'rb') as f:
 		paragraphIndex = row[3]
 		sentenceIndex = row[4]
 		sentenceText = row[5]
-		originalSentenceText = row[6]
+		#originalSentenceText = row[6]
+	
+		#create a dummy sentence for each thread too
+		if (lastSeen != threadID):
+			print 'Sentence.create(thread_id: "{0}", answer_id: "-1", sentence_text: "-1", technique: "-1")'.format(threadID)
 		
 		print 'Sentence.create(thread_id: "{0}", answer_id: "{1}", sentence_text: "{2}", technique: "{3}")'.format(threadID, answerID, sentenceText, technique)
-				
+		lastSeen = threadID
